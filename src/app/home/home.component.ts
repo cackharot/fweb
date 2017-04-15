5cf261c454ca74c9a7bcc7bfe004ab8d3398a64e
cackharot <cackharot@gmail.com>
2017-04-15 10:53:57 -0400

    update logo link

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  searchText: string;
+
+  constructor(private router: Router) {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  search(searchText: string) {
+    this.router.navigate(['search', searchText]);
+  }
 }
